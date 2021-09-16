40c4c55b2b724312dad90c476ed4c05e2ba96741
Mateusz <mat.sz.paw@gmail.com>
2021-09-16 23:59:19 +0200

    aaaaaaaaa

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Button} from './przycisk.js';
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       count: 1
     }
-
   }
-
   handleCount(value) {
     this.setState((prevState) => ({ count: prevState.count + value }));
   }
-
   render() {
-
-
-    return (
+    return(
       <div>
-        Current count: {this.state.count}
-        <hr />
+        Current count:{this.state.count}
+        <hr/>
         <Button sign="-" count={this.state.count} updateCount={this.handleCount.bind(this)} />
         <Button sign="+" count={this.state.count} updateCount={this.handleCount.bind(this)} />
       </div>
     );
   }
 }
-
 export default App;
