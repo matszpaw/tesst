32900e81b6a45a0eb7df676f25be0e224978ff56
Mateusz <mat.sz.paw@gmail.com>
2021-09-16 23:27:37 +0200

    dddddddddd

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import App from './App';
 
-const myfirstelement = <h1>Hello React!</h1>
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
 
-ReactDOM.render(myfirstelement, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+render(<App />, document.getElementById('root'));
+
